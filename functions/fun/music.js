3c9d072f22a111fb84018ef02ec9e85ee1d10c04
ScarVite <43887376+ScarVite@users.noreply.github.com>
2019-08-11 15:38:54 +0200

    test2

--- functions/fun/music.js
+++ functions/fun/music.js
@@ -330,7 +330,6 @@ module.exports = {
           { max: 1, time: 10000 }).then(collected => {
             if (collected.first().emoji.name == '👍') {
                 module.exports.streamyt(message, results[0]["link"])
-                jhdvbk
                 return;
         }
         else
